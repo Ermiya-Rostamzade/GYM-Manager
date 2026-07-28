9cfef5381231ea5e9ab363e0a84e58051f9640b2
Aref Daei <aref.daei@outlook.com>
2026-07-29 01:00:19 +0330

    fix: add EnableJpaAuditing annotation to GymManagementApplication class

--- src/main/java/com/gym/management/GymManagementApplication.java
+++ src/main/java/com/gym/management/GymManagementApplication.java
@@ -2,8 +2,10 @@ package com.gym.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class GymManagementApplication {
 
 	public static void main(String[] args) {
