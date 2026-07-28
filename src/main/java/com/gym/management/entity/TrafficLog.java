6203320311b7279bea2b17f7dfa2d837006cc370
Aref Daei <aref.daei@outlook.com>
2026-07-29 02:00:01 +0330

    fix: make user_id column required in TrafficLog entity

--- src/main/java/com/gym/management/entity/TrafficLog.java
+++ src/main/java/com/gym/management/entity/TrafficLog.java
@@ -13,7 +13,7 @@ import java.time.LocalDateTime;
 public class TrafficLog extends BaseEntity {
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     private LocalDateTime checkInTime;
