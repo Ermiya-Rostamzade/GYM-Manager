2db7d5c4a72d5eca6fc7cc55e682039b20f389d9
Aref Daei <aref.daei@outlook.com>
2026-07-29 01:26:47 +0330

    feat: add initial Users entity

---
+++ src/main/java/com/gym/management/entity/Users.java
@@ -0,0 +1,13 @@
+package com.gym.management.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class Users extends BaseEntity {
+}
