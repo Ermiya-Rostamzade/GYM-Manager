d7af2f4077205bf6f183beeb20de6678837e6a01
Ermiya Rostamzadeh <ermiyarostamzadeh@gmail.com>
2026-07-29 19:34:34 +0330

    add User entity

--- src/main/java/com/gym/management/entity/User.java
+++ src/main/java/com/gym/management/entity/User.java
@@ -1,13 +1,38 @@
 package com.gym.management.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.NoArgsConstructor;
+import com.gym.management.entity.enums.Role;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "users")
 public class User extends BaseEntity {
+
+    @Column(name = "mobile_number", nullable = false, unique = true, length = 15)
+    private String mobileNumber;
+
+    @Column(name = "full_name", nullable = false)
+    private String fullName;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Column(name = "is_active", nullable = false)
+    private Boolean isActive = true;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<RolePermission> permissions = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Notification> notifications = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user")
+    private List<UserSubscription> subscriptions = new ArrayList<>();
 }
