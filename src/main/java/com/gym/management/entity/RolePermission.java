46738c68ccc1ac03db072c05033b953914b96dcd
Ermiya Rostamzadeh <ermiyarostamzadeh@gmail.com>
2026-07-29 20:04:27 +0330

    add RolePermission entity

---
+++ src/main/java/com/gym/management/entity/RolePermission.java
@@ -0,0 +1,23 @@
+package com.gym.management.entity;
+
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "roles_permissions")
+@Getter
+@Setter
+@NoArgsConstructor
+public class RolePermission extends BaseEntity{
+
+    @Column(name = "permission_name", nullable = false)
+    private String permissionName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id",  nullable = false)
+    private User user;
+}
