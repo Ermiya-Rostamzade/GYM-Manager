a14faeef4745a3efd7cf1f243901035d7f639a2c
Kinger <ermiyarostamzadeh@gamilcom>
2026-07-29 20:40:06 +0330

    add Payment entity and add @Noargs to User entity

---
+++ src/main/java/com/gym/management/entity/Payment.java
@@ -0,0 +1,35 @@
+package com.gym.management.entity;
+
+import com.gym.management.entity.enums.PaymentStatus;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "payments")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Payment extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "subscription_id", nullable = false)
+    private UserSubscription subscription;
+
+    @Column(nullable = false)
+    private BigDecimal amount;
+
+    @Column(name = "ref_code")
+    private String refCode;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PaymentStatus status;
+
+    @Column(name = "paid_at")
+    private LocalDateTime paidAt;
+}
