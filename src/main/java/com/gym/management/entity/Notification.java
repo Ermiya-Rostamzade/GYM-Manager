14f0083d7a9cbf11b9b0503535abecdaccff38de
Kinger <ermiyarostamzadeh@gamilcom>
2026-07-29 20:10:09 +0330

    add Notifiaciton entity

---
+++ src/main/java/com/gym/management/entity/Notification.java
@@ -0,0 +1,21 @@
+package com.gym.management.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "notifications")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Notification extends BaseEntity {
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String message;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
