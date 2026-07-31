7f2221cdd30e6310467e79a37ef96b56d8958453
Aref Daei <aref.daei@outlook.com>
2026-07-31 14:10:46 +0330

    feat: add TrafficLogRepository

---
+++ src/main/java/com/gym/management/repository/TrafficLogRepository.java
@@ -0,0 +1,12 @@
+package com.gym.management.repository;
+
+import com.gym.management.entity.TrafficLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TrafficLogRepository extends JpaRepository<TrafficLog, Long> {
+
+    List<TrafficLog> findByUserId(Long userId);
+
+}
