71b93f3b73b257223a395adda9381e4270ce7737
Ermiya Rostamzadeh <ermiyarostamzadeh@gmail.com>
2026-08-04 13:13:18 +0330

    feat(dto): add response DTOs

---
+++ src/main/java/com/gym/management/dto/response/PlanResponse.java
@@ -0,0 +1,14 @@
+package com.gym.management.dto.response;
+
+import com.gym.management.entity.enums.PlanType;
+
+import java.math.BigDecimal;
+
+public record PlanResponse (
+        Long id,
+        String title,
+        BigDecimal price,
+        Integer sessions,
+        Integer durationDays,
+        PlanType planType
+){}
