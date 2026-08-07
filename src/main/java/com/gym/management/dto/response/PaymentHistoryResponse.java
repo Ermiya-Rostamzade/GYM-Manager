516d302d4443d4d1fa7200d33288292f678be2df
Kinger <ermiyarostamzadeh@gamilcom>
2026-08-07 17:47:34 +0330

    feat: add PaymentHistoryResponse to package dto.response

---
+++ src/main/java/com/gym/management/dto/response/PaymentHistoryResponse.java
@@ -0,0 +1,15 @@
+package com.gym.management.dto.response;
+
+import com.gym.management.entity.enums.PaymentStatus;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public record PaymentHistoryResponse(
+        Long id,
+        BigDecimal amount,
+        String refCode,
+        PaymentStatus status,
+        LocalDateTime paidAt,
+        Long subscriptionId //شناسه اشتراکی که برای آن پرداخت انجام شده
+){}
