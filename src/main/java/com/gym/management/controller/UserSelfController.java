c11e708a4f397dc75fb9a9b73c5bec17c279b892
Kinger <ermiyarostamzadeh@gamilcom>
2026-08-24 01:54:56 +0330

    feat: add UserSelfController

---
+++ src/main/java/com/gym/management/controller/UserSelfController.java
@@ -0,0 +1,41 @@
+package com.gym.management.controller;
+
+import com.gym.management.dto.request.UserRegisterRequest;
+import com.gym.management.entity.enums.Role;
+import com.gym.management.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/register")
+@RequiredArgsConstructor
+public class UserSelfController {
+
+    private final UserService userService;
+
+    @GetMapping
+    public String ShowRegisterForm(Model model) {
+        model.addAttribute("user", new UserRegisterRequest(null,null,null, Role.ATHLETE));
+        return "register";
+    }
+    @PostMapping
+    public String RegisterUser(@Valid @ModelAttribute("user") UserRegisterRequest userRegisterRequest , BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "register";
+        }
+        try {
+            userService.registerSelf(userRegisterRequest);
+            return "redirect:/login?registered";
+        }catch (IllegalArgumentException ex){
+            bindingResult.rejectValue("mobileNumber", "error.user", ex.getMessage());
+            return "register";
+        }
+    }
+}
