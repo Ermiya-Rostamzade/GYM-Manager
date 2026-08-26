e19d8967e30e9daf6ac4347687a42e195a343dab
Kinger <ermiyarostamzadeh@gamilcom>
2026-08-26 18:39:34 +0330

    refactor; change name config package to security

--- src/main/java/com/gym/management/security/SecurityConfig.java
+++ src/main/java/com/gym/management/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.gym.management.config;
+package com.gym.management.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
