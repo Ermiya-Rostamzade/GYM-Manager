003d688a919c6d89f6c0d36b27d6f37b6ed8616e
Aref Daei <aref.daei@outlook.com>
2026-08-29 21:52:13 +0330

    feat: Create TrafficLogUserRequest DTO

---
+++ src/main/java/com/gym/management/dto/request/TrafficLogUserRequest.java
@@ -0,0 +1,13 @@
+package com.gym.management.dto.request;
+
+import com.gym.management.entity.enums.TrafficLogMethod;
+import jakarta.validation.constraints.NotNull;
+
+public record TrafficLogUserRequest(
+        @NotNull(message = "user_id is required")
+        Long userId,
+
+        @NotNull(message = "the type of method should be defined")
+        TrafficLogMethod method
+) {
+}
