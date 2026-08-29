15e673120c907509c6c4c4aad53db73b131f042a
Aref Daei <aref.daei@outlook.com>
2026-08-30 00:33:51 +0330

    feat: implement TrafficLogController and add traffic log views

---
+++ src/main/java/com/gym/management/controller/TrafficLogController.java
@@ -0,0 +1,56 @@
+package com.gym.management.controller;
+
+import com.gym.management.dto.request.TrafficLogUserRequest;
+import com.gym.management.entity.enums.TrafficLogMethod;
+import com.gym.management.service.TrafficLogService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/traffic-log")
+@RequiredArgsConstructor
+public class TrafficLogController {
+
+    private final TrafficLogService trafficLogService;
+
+    @GetMapping
+    public String trafficLogs(Model model) {
+        model.addAttribute(
+                "trafficLogs", trafficLogService.getAllTrafficLogs(10)
+        );
+        return "traffic-logs";
+    }
+
+    @GetMapping("/check-in")
+    public String showCheckInQRCode(Model model) {
+        model.addAttribute(
+                "trafficLogCheckIn",
+                new TrafficLogUserRequest(null, TrafficLogMethod.QR_CODE)
+        );
+        return "traffic-log-check-in";
+    }
+
+    @PostMapping("/check-in")
+    public String checkInByQRCode(
+            @Valid @ModelAttribute("trafficLogCheckIn") TrafficLogUserRequest request
+    ) {
+        trafficLogService.createTrafficLog(request);
+        return "redirect:/traffic-log";
+    }
+
+    @GetMapping("/check-out/{id}")
+    public String showCheckOutQRCode(@PathVariable Long id, Model model) {
+        model.addAttribute("trafficLogId", id);
+        return "traffic-log-check-out";
+    }
+
+    @PostMapping("/check-out/{id}")
+    public String checkOutByQRCode(@PathVariable Long id) {
+        trafficLogService.setTrafficLogCheckOutTime(id);
+        return "redirect:/traffic-log";
+    }
+
+}
