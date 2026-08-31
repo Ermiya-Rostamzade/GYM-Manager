790c4aa12252f68eb37b08eface659a3578d087f
Kinger <ermiyarostamzadeh@gamilcom>
2026-08-31 17:16:15 +0330

    feat: add subscriptionController

---
+++ src/main/java/com/gym/management/controller/SubscriptionController.java
@@ -0,0 +1,58 @@
+package com.gym.management.controller;
+
+
+import com.gym.management.dto.request.BuySubscriptionRequest;
+import com.gym.management.dto.response.UserResponse;
+import com.gym.management.service.PlanService;
+import com.gym.management.service.SubscriptionService;
+import com.gym.management.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
+
+@Controller
+@RequestMapping("/subscriptions")
+@RequiredArgsConstructor
+public class SubscriptionController {
+
+    private  final SubscriptionService subscriptionService;
+    private  final PlanService planService;
+    private  final UserService userService;
+
+    @GetMapping("/buy")
+    public String showBuyForm(Model model){
+        model.addAttribute("plans",planService.getAllPlans());
+        return "buy-plan.html";
+    }
+
+    @PostMapping("/purchase")
+    public String purchasePlan(
+            @ModelAttribute BuySubscriptionRequest request,
+            Principal principal,
+            Model model
+    ){
+        UserResponse user = userService.getUserByMobileNumber(principal.getName());
+        try{
+            subscriptionService.buyAndActivateDirectly(user.id(),  request);
+            return "redirect:/subscriptions/my-subscriptions?success";
+        }catch(IllegalArgumentException | IllegalStateException e){
+            return  "redirect:/subscriptions/my-subscriptions?error" + e.getMessage();
+        }
+    }
+
+    @GetMapping("/my-subscriptions")
+    public String showMySubscriptions(Model model, Principal principal){
+        UserResponse user = userService.getUserByMobileNumber(principal.getName());
+        model.addAttribute("user",user);
+        model.addAttribute("subscriptions", subscriptionService.getUserSubscriptions(user.id()));
+        return "my-subscriptions";
+    }
+
+
+}
