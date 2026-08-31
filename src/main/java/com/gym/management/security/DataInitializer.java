6e74f30c361b883a537e5b025bf45147c2b2be81
Ermiya Rostamzadeh <ermiyarostamzadeh@gmail.com>
2026-08-31 17:26:51 +0330

    feat: add DataInitializer class for admin initialization

---
+++ src/main/java/com/gym/management/security/DataInitializer.java
@@ -0,0 +1,35 @@
+package com.gym.management.config;
+
+import com.gym.management.entity.User;
+import com.gym.management.entity.enums.Role;
+import com.gym.management.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class DataInitializer implements CommandLineRunner {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public void run(String... args) {
+        String adminMobile = "09000000000";
+
+
+        if (!userRepository.existsByMobileNumber(adminMobile)) {
+            User admin = new User();
+            admin.setFullName("Super Admin");
+            admin.setMobileNumber(adminMobile);
+            admin.setPassword(passwordEncoder.encode("admin123"));
+            admin.setRole(Role.ADMIN);
+            admin.setIsActive(true);
+
+            userRepository.save(admin);
+            System.out.println(">>> Default Admin Created: Mobile=09000000000 | Password=admin123");
+        }
+    }
+}
